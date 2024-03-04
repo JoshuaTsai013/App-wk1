17aaa4e7f36ebbfae502e86764d699dfc942a205
joshuatsai <joshuatsai1213@gmail.com>
2024-03-04 21:28:23 +0800

    1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>數位三甲 蔡曜宇 111019044</Text>
       <StatusBar style="auto" />
     </View>
   );
